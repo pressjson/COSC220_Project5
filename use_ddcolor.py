0fcc7e986be62b5d1c46d242281ad8b2e40ea796
Jason Press <therealcannedtuna@gmail.com>
2026-04-04 23:54:29 -0700

    Changed defaults, updated READMEs, added some more example output images

--- use_ddcolor.py
+++ use_ddcolor.py
@@ -71,14 +71,14 @@ class DDColorPILPipeline:
 
 if __name__ == "__main__":
     pipe = DDColorPILPipeline(
-        model_name="ddcolor_artistic",
+        model_name="ddcolor_modelscope",
     )
 
     img = Image.open("images/IMG_0956_sklearn_grayscale.JPEG")
     colorized = pipe(img)
     # print("Finished colorizing the image!")
     # print(colorized)
-    save_path = "IMG_0956_ddcolor_artistic.JPEG"
+    save_path = "IMG_0956_ddcolor.JPEG"
     # print(f"Saving to {save_path}")
     colorized.save(save_path)
     # colorized.show()
